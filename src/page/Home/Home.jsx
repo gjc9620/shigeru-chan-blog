5534edd57ff45dddedf0c85e9d6afbf56227dccf
gjc9620 <992758355@qq.com>
2018-05-01 23:35:58 +0800

    name

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -20,26 +20,30 @@ class Header extends React.Component {
           <HR />
           <div className='introduce'>
             <div>Welcome to my commission info site!</div>
-            <div>Please read through everything before</div>
+            <div>Please read my Term of Service before</div>
             <div>ordering or signing up for my waiting list.</div>
+            <div>Thank you! ^_^</div>
+
           </div>
           <div className='backboard'>
             <div>
               <span className='key'>Status:</span>
-              <span className='name'>Closed</span>
+              <span className='name'>Open</span>
             </div>
             <div>
               <span className='key'>Waitlist:</span>
-              <span className='name'>Closed</span>
+              <span className='name'>Open</span>
             </div>
           </div>
           <div className='about-me'>
             <h5>Find me at:</h5>
             <div className='hr' />
-            <p>Twitter: @<a href="">yulitanbo</a></p>
-            <p>Tumblr: <a href="">http://yulipo.tumblr.com</a></p>
-            <p>Pixiv: <a href="">http://pixiv.me/snd11</a></p>
-            <p>Contact: <a href="">yulitanbo@gmail.com</a></p>
+            <p>Email: <a href="mailto:2546113944@qq.com">2546113944@qq.com</a></p>
+            <p>Deviantart: <a href="http://shigeru-chan.deviantart.com">shigeru-chan</a></p>
+            <p>Patreon: <a href="https://www.patreon.com/shigeruchan">shigeruchan</a></p>
+            <p>Instagram: <a href="https://instagram.com/shigerunagato/">shigerunagato</a></p>
+            <p>Tumblr: <a href="https://www.shigeru-chan.tumblr.com">shigerunagato</a></p>
+            <p>Twitter: <a href="https://twitter.com/animeotakudesu">animeotakudesu</a></p>
           </div>
         </div>
         
